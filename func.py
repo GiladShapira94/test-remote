4d099b6aa784fd19f4fc72a56d964844e6005d4e
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2025-01-26 16:16:01 +0200

    Update func.py

--- func.py
+++ func.py
@@ -3,7 +3,7 @@ import mlrun
 
 def handler(context,event):
   project = mlrun.get_or_create_project("test-workflow-gilad")
-  project.run(workflow_path="./workflow.py")
+  project.run(workflow_path="workflow.py")
 
 
 
