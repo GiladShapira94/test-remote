618c3eaa5d56573114d031020f95f2c148ede04b
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2025-01-26 17:59:29 +0200

    Update func.py

--- func.py
+++ func.py
@@ -1,8 +1,8 @@
 
 import mlrun
 import os
-os.chdir("/opt/nuclio")
 def handler(context,event):
+  os.chdir("/opt/nuclio")
   project = mlrun.get_or_create_project("test-workflow-gilad")
   context.logger.info(os.getcwd())
   project.run(workflow_path="workflow.py")
